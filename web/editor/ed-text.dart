09fe84440fcdafc88e0ad6950d56a2be6b03fa68
Michael Mosmann <michael@mosmann.de>
2015-01-05 18:20:37 +0100

    keys working

--- web/editor/ed-text.dart
+++ web/editor/ed-text.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:convert';
 import 'package:polymer/polymer.dart';
 import 'package:paper_elements/paper_slider.dart';
 import 'package:paper_elements/paper_button.dart';
@@ -8,8 +9,33 @@ import 'package:core_elements/core_toolbar.dart';
 
 @CustomTag('ed-text')
 class EditorText extends PolymerElement {
+  @observable String text="Fuuu";
   
-  EditorText.created() : super.created();
+  EditorText.created() : super.created() {
+  }
   
+  HtmlElement textElem;
   
+  void attached() {
+    textElem = $['text'];
+    //textElem.onKeyDown.listen(keyHandler);
+    //textElem.onKeyUp.forEach(listener);
+  }
+  
+  void keyHandler(KeyboardEvent e) {
+    text=text+":Key";
+  }
+  
+  void edit(KeyboardEvent ke,var data, Node parent) {
+    String append=new String.fromCharCodes([ke.keyCode]);
+    text=text+":"+append;
+  }  
+
+  void clicked(MouseEvent e) {
+    text="Maus";
+  }  
+  
+  void onKeyUpX(ElementStream<KeyboardEvent> events) {
+    
+  }
 }
